af1ceab51b4998a4b8f71184708b98bd5e49a81d
SimonV839 <167682180+SimonV839@users.noreply.github.com>
2024-07-23 13:38:53 +0100

    terminate

--- resources/js/genprimes.js
+++ resources/js/genprimes.js
@@ -1,13 +1,20 @@
+
 // Listen for messages from the main thread.
 // If the message command is "generate", call `generatePrimes()`
 addEventListener("message", (message) => {
   if (message.data.command === "generate") {
-    generatePrimes(message.data.quota);
+    generatePrimes(message.data.quota, message.data.command);
   }
+  else
+  {
+    const primes = [];
+    postMessage({primes: primes, message: message.data.command});
+    }
 });
 
+
 // Generate primes (very inefficiently)
-function generatePrimes(quota) {
+function generatePrimes(quota, mes) {
   function isPrime(n) {
     for (let c = 2; c <= Math.sqrt(n); ++c) {
       if (n % c === 0) {
@@ -18,18 +25,18 @@ function generatePrimes(quota) {
   }
 
   const primes = [];
-  //const maximum = 1000000;
-  const maximum = 777;
+  const maximum = 1000000;
 
   while (primes.length < quota) {
     const candidate = Math.floor(Math.random() * (maximum + 1));
     if (isPrime(candidate)) {
       primes.push(candidate);
+      postMessage({primes: primes, message: "working"});
     }
   }
 
   // When we have finished, send a message to the main thread,
   // including the number of primes we generated.
   //postMessage(primes.length);
-  postMessage(primes);
+  postMessage({primes: primes, message: mes});
 }
